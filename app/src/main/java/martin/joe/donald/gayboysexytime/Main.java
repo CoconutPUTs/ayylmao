057a11c789d3550412acb94aa5c9dae409f40c19
Panic Stevenson <panic.stevenson@gmail.com>
2016-04-10 17:21:50 -0500

    lel
    nerd

--- app/src/main/java/martin/joe/donald/gayboysexytime/Main.java
+++ app/src/main/java/martin/joe/donald/gayboysexytime/Main.java
@@ -1,23 +1,44 @@
 package martin.joe.donald.gayboysexytime;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
 
 public class Main extends AppCompatActivity {
+    ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_scrolling_activityp);
-
+        setContentView(R.layout.main);
+        addListenerOnButton();
 
 
         AdView mAdView = (AdView) findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
+    }
+
+    public void addListenerOnButton() {
+        imageButton = (ImageButton) findViewById(R.id.imageButton);
+        imageButton.setOnTouchListener(new View.OnTouchListener() {
+            float scale = 1.0f;
+
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                if (event.getAction() != MotionEvent.ACTION_BUTTON_RELEASE) {
+                    scale = scale * 1.1f;
+                    imageButton.setScaleX(scale);
+                    imageButton.setScaleY(scale);
+                }
+                return false;
+            }
+        });
     }
 }
